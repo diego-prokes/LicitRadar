ab9f243df358145f36697787d4955505936d5f42
Diego Prokes Herbage <diego.prokes@gmail.com>
2024-01-09 20:52:23 -0300

    se modificó el mensaje al finalizar el programa

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def main():
     send_to_sharepoint(licitaciones)
 
 def end_program():
-    input("Presiona cualquier enter para finalizar...")
+    input("Presiona enter para finalizar...")
 
 
 # Verifica si este script está siendo ejecutado directamente
