f97d9ec3062fbbc6ebe4e4433c98fa07c6697618
Diego Prokes Herbage <diego.prokes@gmail.com>
2024-01-11 09:31:04 -0300

    se modificó el texto que se despliega en la terminal al ejecutarlo

--- src/scraper.py
+++ src/scraper.py
@@ -11,6 +11,7 @@ def scrap_mercado_publico():
     que cumplen con ciertas palabras clave.
     '''
     try:
+        print("Scraping...")
         # Obtener las fechas de los últimos <n_days> días utilizando la función get_dates
         n_days = 7
         dates = get_dates(n_days)
